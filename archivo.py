5c278702a137cf98b2828add8f6447d0506ca2dd
Franco Alberti <Albertifranco1997@hotmail.com>
2022-10-22 06:45:12 -0300

    se encapsuló

--- archivo.py
+++ archivo.py
@@ -2,11 +2,16 @@
 
 class Persona():
     def __init__(self, nombre, apellido, edad):
-        nombre = self.nombre
-        apellido = self.apellido
-        edad = self.edad
+        
+        self.__nombre = nombre
+        self.edad = edad
+        self.apellido = apellido
 
     def __str__(self):
         return("hola")
 
-    
+
+
+a = Persona("franco", "alberti", 15)
+
+print(a.apellido)
