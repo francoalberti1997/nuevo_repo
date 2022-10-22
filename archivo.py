de1a83858e36b796ab85a3d6fcb8e48b22ff05b2
Franco Alberti <Albertifranco1997@hotmail.com>
2022-10-22 07:05:35 -0300

    nueva rama con cambio de nombre de clase

--- archivo.py
+++ archivo.py
@@ -1,6 +1,16 @@
 
 
-class Persona():
+class Familia():
     def __init__(self, nombre, apellido):
-        nombre = self.nombre
-        apellido = self.apellido
+        self.nombre = nombre
+        self.apellido = apellido  
+        self.parentesco = True
+    
+    def familiar (self):
+        if self.parentesco:
+            return("es amigo")
+
+
+victor = Familia("victor", "ortiz")
+
+print(victor.familiar())
